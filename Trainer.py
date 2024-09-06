5bc3ed93fab3b1882a5791b127e1e3de8badc2b2
jmerigot <julesmerigot@gmail.com>
2024-09-06 12:15:23 +0200

    edit Trainer

--- Trainer.py
+++ Trainer.py
@@ -21,6 +21,8 @@ from Dataloaders import time_series_module
 """
 Where all the magic happens.
 This is the file that should be run to train the model.
+
+We use a Lightining Trainer, for more info see: https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.trainer.trainer.Trainer.html
 """
 
 
@@ -32,7 +34,7 @@ early_stopping_callback = EarlyStopping(
     mode='min'  # mode can be 'min', 'max', or 'auto'. 'min' means training will stop when the quantity monitored has stopped decreasing
 )  
 
-
+# define Lightning trainer
 trainer = L.Trainer(
     max_epochs=200,
     callbacks=[early_stopping_callback],
@@ -42,15 +44,19 @@ trainer = L.Trainer(
 )
 
 
+# load model
 model = SLA_Lightning_Module(models_dict, model_name, **model_params)
 
+# load data
 data_module = time_series_module(dataloader_config)
 data_module.setup()
 
+# prepate datasets
 train_set, train_loader = data_module.train_dataloader()
 val_set, val_loader = data_module.val_dataloader()
 test_set, test_loader, test_mean, test_std = data_module.test_dataloader()
 
 
+# launch training
 trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 print("Training completed!")
